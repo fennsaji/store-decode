965e4c07ad147ea625ae15eeb8200b305d9bb965
Fenn Ignatius Saji <fennig1997@gmail.com>
2022-12-21 16:53:19 +0530

    Added env as cmd line arg

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {connection} = require("mui-metablockchain-sdk");
+const { connection } = require("mui-metablockchain-sdk");
 const balance = require("./src/balance");
 const did = require("./src/did");
 const vc = require("./src/vc");
@@ -6,11 +6,43 @@ const token = require("./src/token");
 const council = require("./src/council");
 const validator_set = require("./src/validator_set");
 const fs = require('fs');
+const yargs = require('yargs');
 
-function main() {
-    let fork = fs.readFileSync('data/fork.json');
+const argv = yargs
+    .command('deploy', 'Prepare for deployment', {
+        env: {
+            description: 'Environment (devnet / protonet / mainnet)',
+            alias: 'e',
+            type: 'string'
+        },
+    })
+    .help()
+    .alias('help', 'h').argv;
+
+let env = argv.env || argv.e;
+switch(env) {
+    case 'dev': 
+    case 'devnet': 
+    case 'testnet': 
+        env = 'devnet';
+        break;
+    case 'demo': 
+    case 'demonet': 
+    case 'protonet': 
+        env = 'protonet';
+        break;
+    case 'main': 
+    case 'mainnet': 
+        env = 'mainnet';
+        break;
+    default: 
+        throw new Error("Unknown Env. Supported Env: devnet/ protonet/ mainnet")
+}
+
+function main(env) {
+    let fork = fs.readFileSync(`data/${env}/fork.json`);
     let forkJson = JSON.parse(fork).genesis.raw.top;
-    
+
     let balanceStore = balance.getBalances(forkJson);
     console.log("Balances Store Decoded");
 
@@ -30,9 +62,9 @@ function main() {
     console.log("Validator Set Store Decoded");
 
 
-    fs.writeFileSync('data/state.json', JSON.stringify({balanceStore, didStore, vcStore, tokenStore, councilStore, validatorSetStore}));
-    
+    fs.writeFileSync(`data/${env}/state.json`, JSON.stringify({ balanceStore, didStore, vcStore, tokenStore, councilStore, validatorSetStore }));
+
     process.exit(0);
 }
 
-main();
+main(env);
